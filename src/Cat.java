623100299b9769a4ae1794fbd671552c30d99f97
gigachad2005 <kingksaman@gmail.com>
2024-06-28 19:54:23 +0200

    7.2 comit

---
+++ src/Cat.java
@@ -0,0 +1,20 @@
+public class Cat extends Animal implements Moveable {
+    public Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    public String getType() {
+        return "Cat";
+    }
+
+    @Override
+    public void start() {
+        System.out.println(getName() + " starts moving.");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println(getName() + " stops moving.");
+    }
+}
